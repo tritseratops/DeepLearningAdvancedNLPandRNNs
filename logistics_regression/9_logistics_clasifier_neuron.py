ea80129f65e95cbd21203b0877c158f86ab79a93
Oleksandr Yanov <oleksandr.yanov@gmail.com>
2022-05-11 08:41:25 +0300

    added example of neuron work

--- logistics_regression/9_logistics_clasifier_neuron.py
+++ logistics_regression/9_logistics_clasifier_neuron.py
@@ -3,12 +3,11 @@ import numpy as np
 N = 100
 D = 2
 
-X = np.random.randn(N,D)
+X = np.random.randn(N, D)
 ones = np.array([[1]*N]).T
+Xb = np.concatenate((ones, X), axis =1)
 
-Xb = np.concatenate((ones, X), axis=1)
-
-w = np.random.randn(D+1)
+w = np.random.randn(D +1)
 
 Y = Xb.dot(w)
 
@@ -16,4 +15,3 @@ def sigmoid(z):
     return 1/(1+np.exp(-z))
 
 print(sigmoid(Y))
-
