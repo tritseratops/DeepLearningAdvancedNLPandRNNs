78864e6de875d088ab5eda2e86d87faf02c1471b
Oleksandr Yanov <oleksandr.yanov@gmail.com>
2022-05-31 08:51:46 +0300

    finished cross entropy from mind

--- logistics_regression/l20logistics_cross_error_function.py
+++ logistics_regression/l20logistics_cross_error_function.py
@@ -1,54 +1,53 @@
 import numpy as np
 
-# initializing input data
-N=100
+N = 100
 D = 2
 
-X = np.random.randn(N,D)
-
-# first 50 center -2  second 50 center +2
-X[:50, :] = X[:50, :] - 2*np.ones((50,D))
-X[50:, :] = X[50:, :] + 2* np.ones((50,D))
-
-Y = np.zeros(N).T
-Y[:50] = 1
-
-# bias?
+X = np.random.randn(N, D)
+X[:50, :] = X[:50, :] + 2*np.ones((50, D))
+X[50:, :] = X[50:, :] + -2*np.ones((50, D))
 ones = np.ones((N,1))
-# ones = np.array([1]*N).T
+# print("ones:",ones)
+# print("X:",X)
+Xb = np.concatenate((ones, X), axis=1)
 
-Xb= np.concatenate((ones, X), axis=1)
+T = np.ones(N)
+T[50:]=0
 
-
-
-# get sigmoid
 def sigmoid(z):
-    return 1/(1+np.exp(z))
+    return 1/(1 + np.exp(-z))
 
-# calculate cross entropy error
-def cross_extropy_error(T, Y):
+def cross_entropy_error(Yh, Y):
     E = 0
-    items = T.shape[0]
-    for n in range(items):
-        if T[n]==1:
-            E -=np.log(Y[n])
+    for i in range(Yh.shape[0]):
+        if Y[i]==1:
+            E -= np.log(Yh[i])
         else:
-            E -= np.log(1-Y[n])
+            E -=np.log(1-Yh[i])
     return E
 
 # init random weights
-w = np.random.randn(D)
+w = np.random.randn(D+1)
 
-w = np.concatenate((w, np.ones(1)))
+# prediction
+z = Xb.dot(w)
 
-# get prediction
-Yh = Xb.dot(w)
+# squeezing to  near 0..1
+Yh = sigmoid(z)
 
-Xs = sigmoid(Y)
+E = cross_entropy_error(Yh, T)
 
-print(cross_extropy_error(Y, Yh))
+print("w:", w, "\nE:", E)
 
-w = np.array([0, 4, 4])
-Yh = Xb.dot(w)
-Xs = sigmoid(Y)
-print(cross_extropy_error(Y, Yh))
+w = np.array([0, 4 , 4])
+
+# prediction
+z = Xb.dot(w)
+
+# squeezing to  near 0..1
+Yh = sigmoid(z)
+
+E = cross_entropy_error(Yh, T)
+
+print("w:", w, "\nE:", E)
+
