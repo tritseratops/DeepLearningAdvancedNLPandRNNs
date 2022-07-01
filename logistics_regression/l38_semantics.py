579ad8d08d31ce16f5b343844587d9b62302f926
Oleksandr Yanov <oleksandr.yanov@gmail.com>
2022-07-01 09:03:43 +0300

    added tokenizing

--- logistics_regression/l38_semantics.py
+++ logistics_regression/l38_semantics.py
@@ -1,11 +1,28 @@
+# This code is for my NLP Udemy class, which can be found at:
+# https://deeplearningcourses.com/c/data-science-natural-language-processing-in-python
+# https://www.udemy.com/data-science-natural-language-processing-in-python
+# It is written in such a way that tells a story.
+# i.e. So you can follow a thought process of starting from a
+# simple idea, hitting an obstacle, overcoming it, etc.
+# i.e. It is not optimized for anything.
+
+# Author: http://lazyprogrammer.me
+
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.utils import shuffle
+import nltk
 
+from nltk.stem import WordNetLemmatizer
+from sklearn.linear_model import LinearRegression
+from bs4 import BeautifulSoup
 
+# puts word in base form
+wordnet_lemmatizer = WordNetLemmatizer()
 
 
 # from http://www.lextek.com/manuals/onix/stopwords1.html
-
+stopwords = set(w.rstrip() for w in open('data/stopwords.txt'))
 
 # note: an alternative source of stopwords
 # from nltk.corpus import stopwords
@@ -13,7 +30,11 @@ import matplotlib.pyplot as plt
 
 # load the reviews
 # data courtesy of http://www.cs.jhu.edu/~mdredze/datasets/sentiment/index2.html
+positive_reviews = BeautifulSoup(open('data/l38/positive.review').read(), features="html5lib")
+positive_reviews = positive_reviews.find_all('review_text')
 
+negative_reviews = BeautifulSoup(open('data/l38/negative.review').read(), features="html5lib")
+negative_reviews = positive_reviews.find_all('review_text')
 
 
 # first let's just try to tokenize the text using nltk's tokenizer
@@ -27,10 +48,42 @@ import matplotlib.pyplot as plt
 # so it might only add noise to our model.
 # so let's create a function that does all this pre-processing for us
 
+def my_tokenizer(s):
+    s = s.lower()
+    tokens = nltk.tokenize.word_tokenize(s)
+    tokens = [t for t in tokens if len(t)>2] # remove short words, they're probably not useful
+    tokens =  [wordnet_lemmatizer.lemmatize(t) for t in tokens ] # put words into base form
+    tokens = [t for t in tokens if t not in stopwords] # remove stopwords
+    return tokens
 
 
 # create a word-to-index map so that we can create our word-frequency vectors later
 # let's also save the tokenized versions so we don't have to tokenize again later
+word_index_map = {}
+current_index = 0
+positive_tokenized = []
+negative_tokenized = []
+original_reviews = []
+
+for review in positive_reviews:
+    tokens = my_tokenizer(review)
+    positive_tokenized.append(tokens)
+    original_reviews.append(review)
+    for token in tokens:
+        if token not in word_index_map:
+            word_index_map[token]=current_index
+            current_index+=1
+
+for review in negative_reviews:
+    tokens = my_tokenizer(review)
+    negative_tokenized.append(tokens)
+    original_reviews.append(review)
+    for token in tokens:
+        if token not in word_index_map:
+            word_index_map[token]=current_index
+            current_index+=1
+
+print("len(word_index_map)", len(word_index_map))
 
 # now let's create our input matrices
 # (N x D+1 matrix - keeping them together for now so we can shuffle more easily later
