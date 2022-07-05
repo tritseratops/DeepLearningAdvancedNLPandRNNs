1ad3917b7923eebbb21aa4b208b8cc4cee196cd2
Oleksandr Yanov <oleksandr.yanov@gmail.com>
2022-07-05 09:04:29 +0300

    added function stubs

---
+++ logistics_regression/s6l40_facial_recognition.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.utils import shuffle
+
+class LogisticModel(object):
+    def __init(self):
+        pass
+
+    def fit(self, X, Y, learning_rate = 1e-6, reg=0., epochs=120000, show_fig=False):
+        pass
+
+    def forward(self, X):
+        pass
+
+    def predict(self, X):
+        pass
+
+    def score(self, X, Y):
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
