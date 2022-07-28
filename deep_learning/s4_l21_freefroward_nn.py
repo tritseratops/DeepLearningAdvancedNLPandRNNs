0771d34783f539145b8bd2989678a2a2e868edf2
Oleksandr Yanov <oleksandr.yanov@gmail.com>
2022-07-28 08:57:21 +0300

    finished forward function

--- deep_learning/s4_l21_freefroward_nn.py
+++ deep_learning/s4_l21_freefroward_nn.py
@@ -9,9 +9,49 @@ X3 = np.random.randn(N, D) + np.array([4,0])
 X = np.concatenate((X1, X2, X3))
 
 Y =  np.array([0]*500+[1]*500+[2]*500)
-plt.scatter(X[:,0], X[:, 1], c=Y)
-plt.show()
+# plt.scatter(X[:,0], X[:, 1], c=Y)
+# plt.show()
 
-print(Y.shape)
+M = 3 # hidden nodes
+K = 3 # output categories
+
+# initializing weights
+def init_weights(D, M, K):
+    W = np.random.randn(D, M) # DxM
+    b = np.random.randn(M) # bias for hidden layers
+    V = np.random.randn(M, K) # MxK
+    c = np.random.randn(K) # bias for output layers
+
+    # print(W)
+    # print(V)
+
+    return W, b, V, c
+
+def sigmoid(a):
+    return 1/(1+np.exp(-a))
+
+def forward(X, W, b, V, c):
+    Z = sigmoid(X.dot(W) + b)
+    Y =  np.exp(Z.dot(V) + c)
+    Y_softmax =  Y/Y.sum(axis=1, keepdims=True)
+    return Y_softmax
+
+def classification_rate(predictions, targets):
+    return np.mean(targets==predictions)
+
+W, b, V, c = init_weights(D, M, K)
+
+Yp = forward(X, W, b, V, c)
+Ypr = np.argmax(Yp, axis=1)
+
+print("Classification rate with random weights:", classification_rate(Ypr, Y))
+# print(Yp.sum(axis=1))
+# print(Yp)
+# print(Yp.shape)
+# print(Ypr.shape)
+# print(Ypr)
+
+
+
 
 exit()
