f178d4a77798903f92c67ae9fff12c3efc3bc072
Oleksandr Yanov <oleksandr.yanov@gmail.com>
2022-08-05 17:23:03 +0300

    finished prediction and cross entropy

--- deep_learning/s6_l46_model.py
+++ deep_learning/s6_l46_model.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from s6_l46_generating_data import generate_data
+
 class deep_model():
     # N- items, D - features, M, hidden dimations, K - output dimentions
     def __init__(self, N, D, M, K):
@@ -11,14 +13,31 @@ class deep_model():
     def softmax(self, X, W, b, V, c):
         Z = np.tanh(X.dot(W)+b)
         Yp =np.exp(Z.dot(V)+c)
-        return Yp.mean()/Yp.Sum(axis=1)
+        return Yp.mean()/Yp.sum(axis=1)
+
 
     def predict(self, X, W, b, V, c):
-        Yp = self.softmax(X, W, b, V, c)
-        K = V.shape[1] # number of outputs
-        Y = np.argmax(Yp)
+        Z = np.tanh(X.dot(W) + b)
+        Yp = np.exp(Z.dot(V) + c)
+        return Yp
 
+
+def cross_entropy_error(T, Y):
+    return np.mean((1-T)*np.log(1-Y)+T*np.exp(Y))
 
 def main():
-    # X, Y = load_data()
-    model = deep_model()
+    X, Y = generate_data()
+    N = X.shape[0]
+    D = X.shape[1]
+    M = X.shape[1]+2
+    K = 1
+    model = deep_model(N, D, M, K)
+
+    Yp = model.predict(X, model.W, model.b, model.V, model.c)
+
+    print("Target:\n", Y)
+    print("Prediction:\n", Yp)
+    print(cross_entropy_error(Y, Yp))
+
+
+main()
