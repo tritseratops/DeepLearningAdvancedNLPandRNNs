828f4eef6a8039959c404b3a88f3baa00b8e7df6
Oleksandr Yanov <oleksandr.yanov@gmail.com>
2022-09-02 20:06:18 +0300

    added code forfor l5 s36, deep learning, fixed few loading and other bugs

--- deep_learning/l5_s36_model.py
+++ deep_learning/l5_s36_model.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.utils import shuffle
 
 def generate_data():
     N = 500
@@ -16,13 +17,13 @@ def generate_data():
     plt.scatter(X[:,0], X[:, 1], c=Y)
     plt.show()
 
+    return X, Y
+
 
 # generate_data()
 
-class model():
-    def __init__(self, N=None, D=None, M=None, K=None, W=None, b=None, V=None, c=None):
-        if N is None:
-            return
+class cat_3_circles_model():
+    def __init__(self, D=None, M=None, K=None, W=None, b=None, V=None, c=None):
         if D is None:
             return
         if M is None:
@@ -30,19 +31,17 @@ class model():
         if K is None:
             return
         if W is None:
-            self.W = np.random.randn(N, D)/np.sqrt(N, D)
-            self.b = np.random.randn(D)/np.sqrt(D)
+            self.W = self.sigmoid(np.random.randn(D, M))
+            self.b = self.sigmoid(np.random.randn(M))
         else:
             self.W = W
             self.b = b
         if V is None:
-            self.V = np.random.randn(N, D)/np.sqrt(N, D)
-            self.c = np.random.randn(D)/np.sqrt(D)
+            self.V = self.sigmoid(np.random.randn(M, K))
+            self.c = self.sigmoid(np.random.randn(K))
         else:
             self.V = V
             self.c = c
-
-
 
     def sigmoid(self, z):
         return 1/(1+np.exp(-z))
@@ -74,7 +73,7 @@ class model():
         self.b = model[-1]
 
     def gradient_step(self, learning_rate, X, T, W, b, V, c):
-        Yp, Z = self.predict()
+        Yp, Z = self.predict(X, W, b, V, c)
         dJdWmd = ((T-Yp).dot(V).dot(Z).dot(Z-1).dot(X)).sum()
         dJdb = ((T-Yp).dot(V).dot(Z).dot(Z-1)).sum()
         dJdVmk = Z.dot(T-Yp).sum()
@@ -86,12 +85,59 @@ class model():
         return W, b, V, c
 
     def fit(self, Xtrain, Ytrain, Xtest, Ytest, learning_rate, epochs):
+        train_costs = []
+        test_costs = []
         for i in range(epochs):
             # get next state
             self.W, self.b, self.V, self.c = self.gradient_step(learning_rate, Xtrain, Ytrain, self.W, self.b, self.V, self.c)
 
-            # calculate new cost
+            Yp, Z  = self.predict(Xtrain, self.W, self.b, self.V, self.c)
 
-            # add cost to log
+            if i%10==0:
+                # calculate new cost
+                train_cost= self.cross_entropy(Yp, Ytrain)
+
+                # add cost to log
+                train_costs.append(i, train_cost)
+                print("Train cost:", train_cost)
+
+                # calculate new cost
+                test_cost = self.cross_entropy(Yp, Ytrain)
+
+                # add cost to log
+                test_costs.append(i, test_cost)
+                print("Test cost:", test_cost)
+
+        return    train_costs, test_costs
+
+def main():
+    X, Y = generate_data()
+    X, Y = shuffle(X, Y)
+    Xtrain = X[:-100, :]
+    Ytrain = X[:-100, :]
+    Xtest = X[-100:, :]
+    Ytest = X[-100:, :]
+
+    N = Xtrain.shape[0]
+    D = Xtrain.shape[1]
+    M = 5 # user selected, inner dimention can be changed
+    K = 3 # we know we have 3 categories
+    learning_rate = 1E-6
+    EPOCHS  = 10000
+
+
+    model = cat_3_circles_model(D=D, M=M, K=K)
+    train_log, test_log  = model.fit(Xtrain, Ytrain, Xtest, Ytest, learning_rate, EPOCHS)
+
+    # plot costs
+    plt.plot(train_log)
+    plt.title("Cross entropy train")
+    plt.plot(test_log)
+    plt.title("Cross entropy test")
+    plt.legend()
+    plt.show()
+
+
+main()
 
 
