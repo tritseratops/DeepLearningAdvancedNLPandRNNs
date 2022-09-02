e7a8dc69b83a54d1cdfd4fdd075f8bec0badd043
Oleksandr Yanov <oleksandr.yanov@gmail.com>
2022-09-02 09:11:01 +0300

    added init, load and save and stub and part code for fit method for l5 s36

--- deep_learning/l5_s36_model.py
+++ deep_learning/l5_s36_model.py
@@ -20,15 +20,29 @@ def generate_data():
 # generate_data()
 
 class model():
-    def __init__(self, N, D, M, K, W, b, V, c):
-        self.N = N
-        self.D = D
-        self.M = M
-        self.K = K
-        self.W = W
-        self.b = b
-        self.V = V
-        self.c = c
+    def __init__(self, N=None, D=None, M=None, K=None, W=None, b=None, V=None, c=None):
+        if N is None:
+            return
+        if D is None:
+            return
+        if M is None:
+            return
+        if K is None:
+            return
+        if W is None:
+            self.W = np.random.randn(N, D)/np.sqrt(N, D)
+            self.b = np.random.randn(D)/np.sqrt(D)
+        else:
+            self.W = W
+            self.b = b
+        if V is None:
+            self.V = np.random.randn(N, D)/np.sqrt(N, D)
+            self.c = np.random.randn(D)/np.sqrt(D)
+        else:
+            self.V = V
+            self.c = c
+
+
 
     def sigmoid(self, z):
         return 1/(1+np.exp(-z))
@@ -40,13 +54,27 @@ class model():
     def predict(self, X, W, b, V, c):
         z = self.sigmoid(X.dot(W)+b)
         Yp = self.softmax(z.dot(V)+c)
-        return Yp
+        return Yp, z
 
     def cross_entropy(self, Yp, T):
         return ((1-T).dot(np.log(1-Yp))+T.dot(Yp)).mean()
 
-    def gradient_step(self, learning_rate, X, T, Z, W, b, V, c):
-        Yp = self.predict()
+    def save(self, filename='data/s5l36_model.csv'):
+        model = self.W
+        # model.append(self.b)
+        model = np.append(model, self.b)
+        model = np.append(model, self.V)
+        model = np.append(model, self.c)
+
+        np.savetxt(filename, model, delimiter=",")
+
+    def load(self, filename='data/s5l36_model.csv'):
+        model = np.loadtxt(filename, delimiter=',')
+        self.W = model[:-1]
+        self.b = model[-1]
+
+    def gradient_step(self, learning_rate, X, T, W, b, V, c):
+        Yp, Z = self.predict()
         dJdWmd = ((T-Yp).dot(V).dot(Z).dot(Z-1).dot(X)).sum()
         dJdb = ((T-Yp).dot(V).dot(Z).dot(Z-1)).sum()
         dJdVmk = Z.dot(T-Yp).sum()
@@ -57,5 +85,13 @@ class model():
         c = c - learning_rate*dJdCk
         return W, b, V, c
 
+    def fit(self, Xtrain, Ytrain, Xtest, Ytest, learning_rate, epochs):
+        for i in range(epochs):
+            # get next state
+            self.W, self.b, self.V, self.c = self.gradient_step(learning_rate, Xtrain, Ytrain, self.W, self.b, self.V, self.c)
+
+            # calculate new cost
+
+            # add cost to log
 
 
