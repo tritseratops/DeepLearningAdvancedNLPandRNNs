finished shapes fixing, need to check prediction and gradient to correct side

deep_learning/s6_l46_model.py
@@ -18,36 +18,43 @@ class deep_model():
 
 
     def predict(self, X, W, b, V, c):
+        #
         Z = np.tanh(X.dot(W) + b)
-        Yp = np.exp(Z.dot(V) + c)
-        return Yp
+        Yp = Z.dot(V) + c
+        return Z, Yp
 
     def get_dJdVm(self, T, Y, Z):
-        return Z.dot(Y-T)
+        return Z.T.dot(Y-T)
 
     def get_dJdc(self, T, Y):
         return (Y-T).sum()
 
     def get_dJdWdm(self, T, Y, V, Z, X):
+        # T = T.reshape(-1, 1)
         return X.T.dot((Y-T).dot(V.T)*(1-Z*Z))
 
     def get_dJdBm(self, T, Y, V, Z, X):
         return (Y-T).dot(V.T)*(1-Z*Z).sum(axis=0)
 
-    def forward(self, X, T, W, b, V, c, learning_rate):
+    def gradient_step(self, X, T, Y, Z, W, b, V, c, learning_rate, reg_1):
         # derivative tanh: dtanh = 1-Y^2
-        newW = W + (1-np.power(np.tanh(X.dot(W) + b), 2))*learning_rate
-        newb = b + (1-np.power(np.tanh(X.dot(W) + b), 2)).sum(axis=1)*learning_rate
-        Z = np.tanh(X.dot(newW) + newb)
-        newV = V + np.exp(Z.dot(V) + c)*learning_rate
-        newc = V + np.exp(Z.dot(V) + c).sum(axis=1)*learning_rate
-        return newW, newb, newV, newc
+        # newW = W + (1-np.power(np.tanh(X.dot(W) + b), 2))*learning_rate
+        T = T.reshape(-1, 1)
+        newW = W - learning_rate*(self.get_dJdWdm(T, Y, V, Z, X)-reg_1*W)
+        # newb = b + (1-np.power(np.tanh(X.dot(W) + b), 2)).sum(axis=1)*learning_rate
+        newB = b - learning_rate*(self.get_dJdBm(T, Y, V, Z, X)-reg_1*b)
+        # Z = np.tanh(X.dot(newW) + newb)
+        # newV = V + np.exp(Z.dot(V) + c)*learning_rate
+        newV = V - learning_rate*(self.get_dJdVm(T, Y, Z)-reg_1*V)
+        # newc = V + np.exp(Z.dot(V) + c).sum(axis=1)*learning_rate
+        newC = c - learning_rate*(self.get_dJdc(T, Y)-reg_1*c)
+        return newW, newB, newV, newC
 
-    def fit(self, Xtrain, Ytrain, Xtest, Ytest, W, b, V, c, epochs=100, learning_rate=1e-5):
+    def fit(self, Xtrain, Ytrain, Xtest, Ytest, W, b, V, c, epochs=100, learning_rate=1e-5, reg1=0):
         for i in range(epochs):
-            W, b, V, c = self.forward(Xtrain, Ytrain, W, b, V, c, learning_rate)
-            Yp = self.predict(Xtest, W, b, V, c)
-            error = cross_entropy_error(Ytest, Yp)
+            Z, Yp = self.predict(Xtrain, W, b, V, c)
+            W, b, V, c = self.gradient_step(Xtrain, Ytrain, Yp, Z, W, b, V, c, learning_rate, reg1)
+            error = cross_entropy_error(Ytrain, Yp)
             print(error)
         return W, b, V, c
 
@@ -79,10 +86,12 @@ def main():
     K = 1
     model = deep_model(D, M, K)
     epochs = 10000
+    learning_rate = 10e-5
+    reg1 = 0.2
 
     Yp = model.predict(X, model.W, model.b, model.V, model.c)
 
-    W, b, V, c = model.fit(Xtrain, Ytrain, Xtest, Ytest, model.W, model.b, model.V, model.c)
+    W, b, V, c = model.fit(Xtrain, Ytrain, Xtest, Ytest, model.W, model.b, model.V, model.c, epochs, learning_rate, reg1)
 
     print("Target:\n", Y)
     print("Prediction:\n", Yp)